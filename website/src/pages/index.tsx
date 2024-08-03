2db56c8a1aff1ce2500546a4832ddd763f44d8af
LeBalz <lebalz@outlook.com>
2024-08-03 15:20:01 +0200

    cleanup unused deps

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 // @ts-ignore
--- website/src/pages/playground.md
+++ website/src/pages/playground.md
@@ -1,5 +1,7 @@
 # Python Playground
 
+Edit and execute the Python code directly in your browser.
+
 ```py live_py title=python__playground.py id=74ad0e89-c086-4d88-af83-f9c93e23078c noCompare
 print('Hello Playground')
 ```
